480f7dc85912187811e23a889f0396c256beb604
Steven <sclausson@gmail.com>
2024-08-10 22:15:58 -0700

    v20240810
    Initial

---
+++ CSAMathApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, welcome to the Math Game!");
+Console.WriteLine("Last updated 2024-8-10");
+System.Console.WriteLine("Press ENTER to continue...");
+Console.ReadLine();
+Console.Clear();
+System.Console.WriteLine("Please select a difficulty: ");
+System.Console.WriteLine("1: Easy difficulty");
+System.Console.WriteLine("2: Medium difficulty");
+System.Console.WriteLine("3: Hard difficulty");
+System.Console.WriteLine("4: Random game");
+
+
+
+
+//Integer Only
+// + - * /
+//User enters difficulty
+//Game has timer
+//FUnction to pick number of questions
+//Random game with random operations
+//store previous games in a List.
+
+
+
+
+Console.ReadLine();
