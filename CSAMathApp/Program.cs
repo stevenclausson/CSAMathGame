12a7ae26f5dc06f4f70ae1d145ad5bcf7e647403
Steven <sclausson@gmail.com>
2024-08-15 08:54:16 -0700

    Unbroke the game
    - Everything in Program right now.
    - Will class it out once working.

--- CSAMathApp/Program.cs
+++ CSAMathApp/Program.cs
@@ -33,12 +33,11 @@ static void ChooseGame()
     int numberOfQuestions = Int32.Parse(Console.ReadLine());
     if (userChoice == "a")
     {
-        Console.WriteLine("Please.");
         AdditionGame(userDifficulty, numberOfQuestions);
     }
     else if (userChoice == "s")
     {
-        //SubtractionGame(userDifficulty, numberOfQuestions);
+        SubtractionGame(userDifficulty, numberOfQuestions);
     }
     else if (userChoice == "m")
     {
@@ -72,10 +71,36 @@ static void AdditionGame(string difficulty, int questions)
     else if (difficulty == "h")
     {
         Console.WriteLine("Hard.");
+        BeginGame(3, questions);
     }
     else
     {
-        Console.WriteLine("Stop");
+        Console.WriteLine("Stop. That's not a valid game.");
+    }
+}
+
+static void SubtractionGame(string difficulty, int questions)
+{
+    if (difficulty == "e")
+    {
+        Console.WriteLine($"{questions} on easy mode? Got it.");
+        Console.WriteLine("Press any key to begin!");
+        Console.ReadLine();
+        BeginGame(4, questions);
+    }
+    if (difficulty == "m")
+    {
+        Console.WriteLine($"{questions} with a little more difficulty, eh? OK.");
+        Console.WriteLine("Press any key to begin!");
+        Console.ReadLine();
+        BeginGame(5, questions);
+    }
+    if(difficulty == "h")
+    {
+        Console.WriteLine($"{questions} on hard mode? Got it.");
+        Console.WriteLine("Press any key to begin!");
+        Console.ReadLine();
+        BeginGame(6, questions);
     }
 }
 
@@ -94,35 +119,457 @@ static void BeginGame(int game, int questions)
 
             Console.WriteLine($"{num1} + {num2}");
             int answer = num1 + num2;
-            int userAnswer = Int32.Parse(Console.ReadLine());
-            if (userAnswer == answer)
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
             {
-                totalCorrect++;
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
             }
-            Console.WriteLine($"Total correct: {totalCorrect}");
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
         }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
     }
     else if (game == 2)
     {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(10, 100);
+            int num2 = rnd.Next(10, 100);
+
+            Console.WriteLine($"{num1} + {num2}");
+            int answer = num1 + num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
 
     }
     else if (game == 3)
     {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(100, 1000);
+            int num2 = rnd.Next(100, 1000);
 
+            Console.WriteLine($"{num1} + {num2}");
+            int answer = num1 + num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+
+    else if (game == 4)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(1, 10);
+            int num2 = rnd.Next(1, 10);
+
+            Console.WriteLine($"{num1} - {num2}");
+            int answer = num1 - num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+
+    }
+
+    else if (game == 5)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(10, 100);
+            int num2 = rnd.Next(100, 1000);
+
+            Console.WriteLine($"{num1} - {num2}");
+            int answer = num1 - num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if (game == 6)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(100, 1000);
+            int num2 = rnd.Next(100, 1000);
+
+            Console.WriteLine($"{num1} - {num2}");
+            int answer = num1 - num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if (game == 7)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(1, 10);
+            int num2 = rnd.Next(1, 10);
+
+            Console.WriteLine($"{num1} * {num2}");
+            int answer = num1 * num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if ( game == 8)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(10, 100);
+            int num2 = rnd.Next(10, 100);
+
+            Console.WriteLine($"{num1} * {num2}");
+            int answer = num1 * num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if ( game == 9)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(100, 1000);
+            int num2 = rnd.Next(100, 1000);
+
+            Console.WriteLine($"{num1} * {num2}");
+            int answer = num1 * num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if (game == 10)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(1, 10);
+            int num2 = rnd.Next(1, 10);
+
+            Console.WriteLine($"{num1} / {num2}");
+            int answer = num1 / num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if(game == 11)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(10, 100);
+            int num2 = rnd.Next(10, 100);
+
+            Console.WriteLine($"{num1} / {num2}");
+            int answer = num1 / num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
+    }
+    else if (game == 12)
+    {
+        sw.Start();
+        Random rnd = new Random();
+        for (int i = 0; i < questions; i++)
+        {
+            int num1 = rnd.Next(100, 1000);
+            int num2 = rnd.Next(100, 1000);
+
+            Console.WriteLine($"{num1} / {num2}");
+            int answer = num1 / num2;
+            bool success = int.TryParse(Console.ReadLine(), out int result);
+            if (success)
+            {
+                if (result == answer)
+                {
+                    totalCorrect++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an answer.");
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+        sw.Stop();
+
+        // Calculates Stopwatch elapsed time
+        TimeSpan ts = sw.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        Console.WriteLine($"Runtime: {elapsedTime}");
+        Console.ReadLine();
+        Console.WriteLine($"Total correct: {totalCorrect}");
     }
 }
 
 
-//Integer Only
-// +
-// - 
-// *
-// /
-//Game has timer
+
+// - Easy Medium Hard
+// * Easy Medium Hard
+// / Easy Medium Hard
 //Random game with random operations
 //store previous games in a List.
 
 //DONE User enters difficulty
 //DONE Function to pick number of questions
+//DONE + 
+//DONE Integer Only
+//DONE Game has timer
 
 
